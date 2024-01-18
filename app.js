d7caf040e9431506a5c5b2312a4dfdc4fad07f33
jordandubreuil <jdubreuil@neit.edu>
2024-01-18 14:41:23 -0500

    End of class week 2

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var path = require('path');
+var mongoose = require('mongoose');
 
+
+//connect to mongodb via mongoose
+mongoose.connect("mongodb://0.0.0.0:27017/Games",{
+}).then(function(){
+    console.log("Connected to MongoDb Database");
+}).catch(function(err){
+    console.log(err);
+}); 
 //var num = 6 *6;
 
 //console.log(num);
@@ -25,6 +34,22 @@ app.get('/about', function(req,res){
 app.get('/contact', function(req,res){ 
     //res.send("here would be the page from the route");
     res.sendFile(path.join(__dirname+"/pages/contact.html"));
+});
+
+var Schema = mongoose.Schema;
+
+var GameData = new Schema({
+    gamename:{
+        type:String
+    }
+});
+
+var gameModel = mongoose.model('gameModel', GameData);
+
+app.get('/getdata',function(req,res){
+    gameModel.find({}).then(function(games){
+        res.json({games});
+    })
 });
 
 app.listen(3000, function(){
